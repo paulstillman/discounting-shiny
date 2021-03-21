71826d2087469b7b1cc894bd423dbaca1855bdc8
Paul Stillman <paulstillman@users.noreply.github.com>
2021-03-21 15:27:59 -0400

    update app

--- app.R
+++ app.R
@@ -1,48 +1,85 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+library(patchwork)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Prospect Theory Parameters"),
+    
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            numericInput("r",
+                         "Exponential Discount Rate (r):",
+                         min = 0,
+                         max = 1,
+                         value = .01,
+                         step = .001),
+            numericInput("k",
+                         "Hyperbolic Discount Rate (k)",
+                         min = 0,
+                         max = 1,
+                         value = .01,
+                         step = .001),
+            numericInput("LL",
+                         "Larger Later",
+                         min = 0,
+                         max = 10000,
+                         value = 100,
+                         step = 10),
+            numericInput("beta",
+                         "beta",
+                         min = 0,
+                         max = 1,
+                         value = .98,
+                         step = .001),
+            numericInput("delta",
+                         "delta",
+                         min = 0,
+                         max = 1,
+                         value = .98,
+                         step = .001)
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            plotOutput("scatPlot")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    calc_exponential <- function(V, D, r) {
+        V / (1 + r)^D
+    }
+    
+    # hyperbolic
+    calc_hyperbolic <- function(V, D, k) {
+        V / (1 + k*D)
+    }
+    
+    calc_quasi_hyperbolic <- function(V, D, beta, delta) {
+        V * (beta * delta^D)
+    }
+    
+    stat_exponential <- reactive(stat_function(mapping = aes(color = 'Exponential'), fun = calc_exponential, size = 1.5, args = list(V = input$LL, r = input$r), alpha = .5))
+    stat_hyperbolic <- reactive(stat_function(mapping = aes(color = 'Hyperbolic'), fun = calc_hyperbolic, size = 1.5, args = list(V = input$LL, k = input$k), alpha = .5))
+    stat_quasihyp <- reactive(stat_function(mapping = aes(color = 'Quasi-Hyperbolic'), fun = calc_quasi_hyperbolic, size = 1.5, args = list(V = input$LL, beta = input$beta, delta = input$delta), alpha = .5))
+    
+    output$scatPlot <- renderPlot({
+        ggplot(data.frame(x=c(0, 360)), aes(x = x)) +
+            stat_exponential() +
+            stat_hyperbolic() +
+            stat_quasihyp() +
+            scale_x_continuous('Delay of Reward (D)') +
+            scale_y_continuous('Present-Day Value') +
+            theme_bw() +
+            theme(text = element_text(size = 16, face = 'bold')) +
+            labs(title = 'Hyperbolic Discounting: Value of $50 as a\nfunction of delay (D) and discount rate (r)')
+        
     })
+    
 }
 
 # Run the application 
